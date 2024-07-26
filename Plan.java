07c66cce86d7380170c8338eab89820b507b58c6
IslandBoy27 <141108469+IslandBoy27@users.noreply.github.com>
2024-07-26 15:39:54 -0600

    De Jaymar
    Este es El Clase "Plan", el cual es el Clase Padre Del Proyecto.

---
+++ Plan.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package herencia_laboratorio2;
+
+/**
+ *
+ * @author Jayma
+ */
+public abstract class Plan {
+
+    private String numeroTel;
+    private String nombre;
+
+    public Plan(String numeroTel, String nombre) {
+        this.numeroTel = numeroTel;
+        this.nombre = nombre;
+    }
+
+    public String getNumeroTel() {
+        return numeroTel;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public abstract double pagoMensual(int mins, int msgs);
+
+    public void imprimir() {
+        System.out.println("Número de Teléfono: " + numeroTel);
+        System.out.println("Nombre: " + nombre);
+    }
+
+    public String toString() {
+        return "Número de Teléfono: " + numeroTel + "\nNombre: " + nombre;
+    }
+}
